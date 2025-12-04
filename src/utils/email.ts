bc2818b917052eb774773ac934bd0194ee97dc62
Deepak Dhakad <145159816+DeepakDkd@users.noreply.github.com>
2025-12-04 22:59:19 +0530

    nodemailer added

---
+++ src/utils/email.ts
@@ -0,0 +1,26 @@
+import nodemailer from 'nodemailer';
+
+export const sendEmail = async ({to, subject, text, html}: {to: string, subject: string, text: string, html: string}): Promise<void> => {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: Number(process.env.EMAIL_PORT),
+            secure: process.env.EMAIL_SECURE === "true",
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            },
+        });
+
+        await transporter.sendMail({
+            from: `ClassSync  <${process.env.EMAIL_FROM}>`,
+            to,
+            subject,
+            text,
+            html,
+        });
+    } catch (error) {
+        console.error("Error sending email:", error);
+        throw new Error("Email sending failed");
+    }
+}
