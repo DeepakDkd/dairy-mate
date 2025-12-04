f81f8b9f1167ce9aa54c2b6770b655a44b356ad5
Deepak Dhakad <145159816+DeepakDkd@users.noreply.github.com>
2025-12-04 23:00:31 +0530

    send otp api

---
+++ src/app/api/auth/send-otp/route.tsx
@@ -0,0 +1,66 @@
+import { prisma } from "@/lib/db";
+import { generateOtpEmail } from "@/templates/email/otp";
+import { sendEmail } from "@/utils/email";
+import { generateOtp, generateSalt, hashOtp } from "@/utils/otp";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    const { phone } = await req.json();
+    try {
+        if (!phone) {
+            return NextResponse.json(
+                { message: "Phone number is required" },
+                { status: 400 }
+            );
+        }
+        const user = await prisma.user.findFirst({
+            where: {
+                phone,
+            }
+        });
+        if (!user) {
+            return NextResponse.json(
+                { message: "User not found" },
+                { status: 404 }
+            );
+        }
+        const otp = generateOtp();
+        const salt = generateSalt();
+        const hashedOtp = hashOtp(otp, salt);
+        const expiry = new Date(Date.now() + 5 * 60 * 1000); // 5 minutes from now
+        
+        await prisma.otpRequest.create({
+            data:{
+                phone,
+                email:user.email,
+                otp: hashedOtp,
+                salt,
+                userId: user.id,
+                expiresAt: expiry,
+            }
+        })
+        
+         const { html, text } = generateOtpEmail(otp, 5, "Dairy Mate");
+
+         await sendEmail({
+            to: phone,
+            subject: "Your OTP Code for Dairy Mate",
+            html,
+            text,
+         })
+
+        return NextResponse.json(
+            { success: true, message: "OTP sent successfully" },
+            { status: 200 }
+        );
+
+
+
+    } catch (error) {
+        console.error("Send OTP error:", error);
+        return NextResponse.json(
+            { message: "Internal server error" },
+            { status: 500 }
+        );
+    }
+}
