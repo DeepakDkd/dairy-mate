c6261474b595b5bd13a0eaeaa223bbf9e1305a88
DeepakDkd <145159816+DeepakDkd@users.noreply.github.com>
2026-05-17 16:58:51 +0530

    refactor: restructure middleware to improve readability and maintainability

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,18 @@
 import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
-export default withAuth({
-  pages: {
-    signIn: "/auth/login",
+export default withAuth(
+  function middleware(req) {
+    if (req.nextUrl.pathname.startsWith("/dashboard")) {
+      return NextResponse.redirect(new URL("/portal", req.url));
+    }
   },
-});
+  {
+    pages: {
+      signIn: "/auth/login",
+    },
+  }
+);
 
 export const config = {
   matcher: [
